480f3b9d18a74165dd4fa59bad869ecc1a7a29b0
mirakaya <mariajonas.sousa00@gmail.com>
2023-11-12 16:07:06 +0000

    Correct implementation + comments.

--- src/sieve_eratosthenes.java
+++ src/sieve_eratosthenes.java
@@ -7,38 +7,61 @@ public class sieve_eratosthenes {
 
     private static LinkedList sieve_eratosthenes(int max_val) {
 
-        LinkedList results = new LinkedList();
+        LinkedList <Integer> aux = new LinkedList();
 
         for (int i = 2; i < max_val; i++){
 
-            results.add(i);
+            aux.add(i);
 
         }
 
 
-        for (int i = 2; i <= max_val; i++){
+        for (int i = 0; i < aux.size(); i++){
 
-            int removed = 0;
+            for (int curr_iteration = 2; aux.get(i) * curr_iteration <= max_val; curr_iteration ++){
 
-            for (int curr_iteration = 2; i * curr_iteration <= max_val; curr_iteration ++){
-
-                if (results.contains(i * curr_iteration) ){
-                    results.remove(Integer.valueOf(i * curr_iteration));
-                    removed ++;
+                if (aux.contains(aux.get(i) * curr_iteration) ){
+                    aux.remove(Integer.valueOf(i * curr_iteration));
                 }
             }
 
         }
 
-        return results;
+        return aux;
 
+    }
+
+
+    private static LinkedList sieveOfEratosthenes_v2 (int n) {
+
+
+        LinkedList<Integer> aux = new LinkedList();
+
+        //Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+        for (int i = 2; i < n; i++) {
+            aux.add(i);
+        }
+
+        //Initially, let p equal 2, the smallest prime number
+        for (int p = 2; p <= n; p++) {
+
+            if (aux.contains(p)) { //then its prime
+
+                //Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list
+                for (int i = p * 2; i <= n; i += p) {
+                    aux.remove(Integer.valueOf(i));
+                }
+            }
+        }
+
+        return aux;
     }
 
     public static void main(String[] args) {
 
         int max = 500;
 
-        LinkedList results = sieve_eratosthenes(max);
+        LinkedList results = sieveOfEratosthenes_v2(max);
 
         System.out.println(results);
 
